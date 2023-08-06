e5a589aa3563f07688da90c06b3a97e89a93c00c
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-06 09:15:46 +0300

    Update gcloud.tf

--- gcloud.tf
+++ gcloud.tf
@@ -14,10 +14,12 @@ resource "google_ml_engine_model" "my_classifier" {
     team        = "data-science"
   }
 
-  default_version {
-    name = "v1"
-    deployment_uri = "gs://my-bucket/trained_model"
-    runtime_version = "1.15"
-    machine_type = "n1-standard-4"
+ online_prediction_logging {
+    node_count = 1
+    python_version = "3.7"
+    package_uris = ["gs://my-bucket/trained_model/my_model-1.0.tar.gz"]
   }
+
+  regions = ["us-central1"]
 }
+
