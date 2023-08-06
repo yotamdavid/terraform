5244b397e5e68c523e39dd69259aab0ed1d1ad7b
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-06 09:33:34 +0300

    Update gcloud.tf

--- gcloud.tf
+++ gcloud.tf
@@ -4,28 +4,9 @@ provider "google" {
   region      = "us-central1"
 }
 
-resource "google_ml_engine_model" "my_classifier" {
-  name     = "test-classifier"
-  project  = "golden-system-393111"
-  regions  = ["us-central1"]
-
-  labels = {
-    environment = "production"
-    team        = "data-science"
-  }
-
- online_prediction_logging {
-    node_count = 1
-    python_version = "3.7"
-    package_uris = ["gs://my-bucket/trained_model/my_model-1.0.tar.gz"]
-  }
-
- default_version {
-    name            = "v1"
-    
-    runtime_version = "1.15"
-    
-    machine_type    = "n1-standard-4"
-  }
-}
+resource "google_ml_engine_model" "test" {
+  enable_autopilot = true
+  display_name    = "test-clusser"
+  project         = "golden-system-393111"
+  regions         = ["us-central1"]
 
